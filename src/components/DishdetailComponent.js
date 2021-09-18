30d25e091657f28ba56c603811b4e337d27fe612
pablocourault <pablocourault@gmail.com>
2021-09-18 12:04:13 -0300

    Header and Footer

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -6,10 +6,10 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
         return (
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={this.props.dish.image} alt={this.props.dish.name} />
+                        <CardImg width="100%" src={dish.image} alt={dish.name} />
                     <CardBody>
-                        <CardTitle>{this.props.dish.name}</CardTitle>
-                        <CardText>{this.props.dish.description}</CardText>
+                        <CardTitle>{dish.name}</CardTitle>
+                        <CardText>{dish.description}</CardText>
                     </CardBody>
                     </Card>
                 </div>
