d41c337ed8891db8b8d5c41372c1157ae367752d
pablocourault <pablocourault@gmail.com>
2021-09-23 21:45:31 -0300

    Single Page Applications Part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,12 +1,13 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from 'react-router-dom';
 
     function RenderDish({dish}) 
       {
         return (
                 <div className="col-12 col-md-5 m-1">
                     <Card>
-                        <CardImg width="100%" src={dish.image} alt={dish.name} />
+                        <CardImg top src={dish.image} alt={dish.name} />
                     <CardBody>
                         <CardTitle>{dish.name}</CardTitle>
                         <CardText>{dish.description}</CardText>
@@ -18,32 +19,26 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
     function RenderComments({comments})
         {
-
-        const list = comments.map((comment) => {
-
-            if (comment != null) 
+            if (comments != null) 
                 {
-                return (            
-                        <li key={comment.id}>
-                        <div className="row"> {comment.comment} </div>
-                        <div className="row"> --&nbsp;{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date))) } </div>  
-                        </li>
-                        )
-                }
-                return null
-            }
-            )
-
-            return (
-                    <div className="col-12 col-md-5 m-1">
-                        <div><h4>Comments</h4></div>
-                        <div> 
+                return (<div className="col-12 col-md-5 m-1">     
+                        <h4>Comments</h4>
                             <ul className="list-unstyled">
-                                {list} 
+                                {comments.map((comment) => {
+                                return (
+                                <li key={comment.id}>
+                                <p> {comment.comment} </p>
+                                <p> --&nbsp;{comment.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date))) } </p>  
+                                </li> );
+                                })}
                             </ul>
                         </div>
-                    </div>
-                    )
+                        )
+                }
+            
+            else
+                return (<div></div>)
+
         }
         
 
@@ -53,9 +48,21 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from "reactstrap";
 
                 return ( 
                          <div className="container">
+                            <div className="row">
+                                <Breadcrumb>
+                                    <BreadcrumbItem> <Link to='/home'>Home</Link> </BreadcrumbItem>
+                                    <BreadcrumbItem> <Link to='/menu'>Menu</Link> </BreadcrumbItem>
+                                    <BreadcrumbItem active> {props.dish.name} </BreadcrumbItem>
+                                </Breadcrumb>
+                                <div className="col-12">
+                                    <h3>{props.dish.name}</h3>
+                                    <hr />
+                                </div>
+                            </div>
+
                              <div className="row">
                                  < RenderDish dish={props.dish} />
-                                 < RenderComments comments={props.dish.comments} />
+                                 < RenderComments comments={props.comments} />
                              </div>
                          </div>
                         );
